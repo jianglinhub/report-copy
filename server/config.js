c4933e4581a4b2f6842b2cfd7cc7fe22da19813d
jianglin <jianglin@jianglindeMacBook-Pro.local>
2018-01-24 11:30:00 +0800

    优化服务端代码

--- server/config.js
+++ server/config.js
@@ -4,20 +4,20 @@ const dashboard_service = process.env.DASHBOARD_SERVICE || 'http://127.0.0.1:301
 const gateway = process.env.GATEWAY || 'http://172.16.88.35:7777/'
 
 let config = {
-  "port": process.env.PORT || "3333",
-  "appinfo": {
-    "dashboard": {
-      "appkey": "c6860080d01911e7a57edff4153c4b77",
-      "secretkey": "6f17d822fd324a018a41c36c88862258"
+  'port': process.env.PORT || '3333',
+  'appinfo': {
+    'dashboard': {
+      'appkey': 'c6860080d01911e7a57edff4153c4b77',
+      'secretkey': '6f17d822fd324a018a41c36c88862258'
     },
-    "gateway": {
-      "appkey": "6839153606",
-      "secretkey": "bcd3edfbc30e64ec46cab5c5fcad6813"
+    'gateway': {
+      'appkey': '6839153606',
+      'secretkey': 'bcd3edfbc30e64ec46cab5c5fcad6813'
     }
   },
-  "dashboard-api": {
-    "host": dashboard_service,
-    "uri": "dashboard-api/"
+  'dashboard-api': {
+    'host': dashboard_service,
+    'uri': 'dashboard-api/'
   }
 }
 config.pkg = packageConfig
